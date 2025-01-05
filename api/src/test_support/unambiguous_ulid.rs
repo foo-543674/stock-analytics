8c5ae11efa71497e4896b66491f5f28271d8dfd4
foo-543674 <foo.avoutar7654@gmail.com>
2025-01-05 17:15:15 +0000

    [doc]translate to Engilish

--- api/src/test_support/unambiguous_ulid.rs
+++ api/src/test_support/unambiguous_ulid.rs
@@ -5,9 +5,8 @@ use base32;
 
 #[cfg(test)]
 pub fn unambiguous_ulid() -> impl Strategy<Value = String> {
-  // タイムスタンプ部分（48ビット: 6バイト）
-
-use core::time;
+  use core::time;
+  // NOTE: Timestamp segment. 48bits (6 bytes) string encoded in base32.
   let timestamp_strategy = proptest::num::u64::ANY.prop_map(|ts| {
     let timestamp = ts % (time::Duration::from_secs(2u64.pow(48)).as_nanos() as u64);
     base32::encode(
@@ -16,12 +15,12 @@ use core::time;
     ).to_uppercase()
   });
 
-  // ランダム部分（80ビット: 10バイト）
+  // NOTE: Random segment. 80bits (10 bytes) string encoded in base32.
   let random_strategy = proptest::collection::vec(proptest::num::u8::ANY, 10).prop_map(|bytes| {
     base32::encode(base32::Alphabet::Crockford, &bytes).to_uppercase()
   });
 
-  // タイムスタンプ部分とランダム部分を結合してULIDを生成
+  // NOTE: Combine timestamp and random segments.
   (timestamp_strategy, random_strategy).prop_map(|(timestamp, random)| {
     format!("{}{}", timestamp, random)
   })
