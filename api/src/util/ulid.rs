bdd074993de5d9bca66ec4b4098372404239ce7a
foo-543674 <foo.avoutar7654@gmail.com>
2025-01-05 17:06:57 +0000

    [refactor]rename StringExt to avoid conflict

--- api/src/util/ulid.rs
+++ api/src/util/ulid.rs
@@ -4,11 +4,11 @@ pub fn default_ulid() -> Ulid {
   Ulid::from_string("00000000000000000000000000").unwrap()
 }
 
-pub trait StringExt {
+pub trait StringExtForUlid {
   fn is_ulid(&self) -> bool;
 }
 
-impl StringExt for str {
+impl StringExtForUlid for str {
   fn is_ulid(&self) -> bool {
     Ulid::from_string(self).is_ok()
   }
