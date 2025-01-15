732d813f9bcc6eb596631d35a55d08135deac955
foo-543674 <foo.avoutar7654@gmail.com>
2025-01-15 15:32:52 +0000

    [chore]export modules

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,4 +1,6 @@
 pub mod applications;
 pub mod domains;
+pub mod https;
+pub mod routes;
 pub mod test_support;
 pub mod util;
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use stock_analytics::routes::router::router;
+
+#[tokio::main]
+async fn main() {
+  tracing_subscriber::fmt::init();
+
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+  axum::serve(listener, router()).await.unwrap();
 }
