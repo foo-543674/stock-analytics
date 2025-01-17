274b19bea223d50f3e6ed7e946f3ad8a7ba43071
foo-543674 <foo.avoutar7654@gmail.com>
2025-01-17 17:26:36 +0000

    [chore]export module

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,9 +1,11 @@
-use stock_analytics::routes::router::router;
+use stock_analytics::{modules::root::RootModule, routes::router::router};
 
 #[tokio::main]
 async fn main() {
   tracing_subscriber::fmt::init();
 
+  let module = RootModule::new();
+
   let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-  axum::serve(listener, router()).await.unwrap();
+  axum::serve(listener, router(module)).await.unwrap();
 }
