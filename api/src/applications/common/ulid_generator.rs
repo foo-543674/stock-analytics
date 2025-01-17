e0c8c9bc904b16abf1e22549bcea099150dfe975
foo-543674 <foo.avoutar7654@gmail.com>
2025-01-17 17:23:38 +0000

    [feat]add UlidGeneratorImpl

--- api/src/applications/common/ulid_generator.rs
+++ api/src/applications/common/ulid_generator.rs
@@ -5,6 +5,14 @@ use mockall::automock;
 use crate::applications::errors::application_error::ApplicationError;
 
 #[cfg_attr(test, automock)]
-pub trait UlidGenerator {
+pub trait UlidGenerator: Sync + Send {
   fn generate(&self) -> Result<Ulid, ApplicationError>;
+}
+
+pub struct UlidGeneratorImpl;
+
+impl UlidGenerator for UlidGeneratorImpl {
+  fn generate(&self) -> Result<Ulid, ApplicationError> {
+    Ok(Ulid::new())
+  }
 }
