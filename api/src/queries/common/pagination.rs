4f828d12d4afa47f979706a9e9f8725302aa954d
foo-543674 <foo.avoutar7654@gmail.com>
2025-01-25 17:20:41 +0000

    [refactor]add calculate feature to patination

--- api/src/queries/common/pagination.rs
+++ api/src/queries/common/pagination.rs
@@ -3,10 +3,6 @@ pub struct PageNumer(u32);
 
 impl PageNumer {
   pub fn new(page: u32) -> Self {
-    if page < 1 {
-      panic!("Page number must be greater than 0");
-    }
-
     PageNumer(page)
   }
 
@@ -31,9 +27,6 @@ const DEFAULT_ITEMS_PER_PAGE: u32 = 10;
 
 impl ItemsPerPage {
   pub fn new(items_per_page: u32) -> Self {
-    if items_per_page < 1 {
-      panic!("Items per page must be greater than 0");
-    }
     if items_per_page > MAX_ITEMS_PER_PAGE {
       panic!("Items per page must be less than or equal to {}", MAX_ITEMS_PER_PAGE);
     }
@@ -54,72 +47,79 @@ impl ItemsPerPage {
   }
 }
 
+pub struct Pagination {
+  pub num: PageNumer,
+  pub size: ItemsPerPage,
+}
+
+impl Pagination {
+  pub fn new(num: u32, size: u32) -> Self {
+    Pagination {
+      num: PageNumer::new(num),
+      size: ItemsPerPage::new(size),
+    }
+  }
+
+  pub fn from_int(num: i32, size: i32) -> Self {
+    Pagination {
+      num: PageNumer::from_int(num),
+      size: ItemsPerPage::from_int(size),
+    }
+  }
+
+  pub fn offset(&self) -> u32 {
+    self.size.value() * (self.num.value() - 1)
+  }
+
+  pub fn limit(&self) -> u32 {
+    self.size.value()
+  }
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
   use proptest::prelude::*;
 
-  #[test]
-  #[should_panic(expected = "Page number must be greater than 0")]
-  fn page_number_should_be_panic_when_value_less_than_1() {
-    PageNumer::new(0);
-  }
-
-  proptest! {
+  proptest!{
     #[test]
-    fn page_number_should_be_ok_when_value_greater_than_0(value in 1u32..=u32::MAX) {
-      let page_number = PageNumer::new(value);
-      assert_eq!(page_number.value(), value);
+    fn pagination_should_create_from_int(num in 1..i32::MAX, size in 1..=100) {
+      let pagination = Pagination::from_int(num, size);
+      assert_eq!(pagination.num.value(), num as u32);
+      assert_eq!(pagination.size.value(), size as u32);
     }
 
     #[test]
-    fn page_number_should_be_as_is_when_value_is_positive(value in 1i32..=i32::MAX) {
-      let page_number = PageNumer::from_int(value);
-      assert_eq!(page_number.value(), value as u32);
+    fn pagination_should_set_1_to_page_num_when_num_is_less_than_1(num in i32::MIN..=0, size in 1..=100) {
+      let pagination = Pagination::from_int(num, size);
+      assert_eq!(pagination.num.value(), 1);
+      assert_eq!(pagination.size.value(), size as u32);
     }
 
     #[test]
-    fn page_number_should_be_1_when_value_is_negative(value in i32::MIN..=0i32) {
-      let page_number = PageNumer::from_int(value);
-      assert_eq!(page_number.value(), 1);
-    }
-  }
-
-  #[test]
-  #[should_panic(expected = "Items per page must be greater than 0")]
-  fn items_per_page_should_be_panic_when_value_less_than_1() {
-    ItemsPerPage::new(0);
-  }
-
-  #[test]
-  #[should_panic(expected = "Items per page must be less than or equal to 100")]
-  fn items_per_page_should_be_panic_when_value_greater_than_100() {
-    ItemsPerPage::new(101);
-  }
-
-  proptest! {
-    #[test]
-    fn items_per_page_should_be_ok_when_value_between_1_and_100(value in 1u32..=100u32) {
-      let items_per_page = ItemsPerPage::new(value);
-      assert_eq!(items_per_page.value(), value);
+    fn pagination_should_set_default_to_size_when_size_is_less_than_1(num in 1..i32::MAX, size in i32::MIN..=0) {
+      let pagination = Pagination::from_int(num, size);
+      assert_eq!(pagination.num.value(), num as u32);
+      assert_eq!(pagination.size.value(), DEFAULT_ITEMS_PER_PAGE);
     }
 
     #[test]
-    fn items_per_page_should_be_as_is_when_value_between_1_to_100(value in 1i32..=100i32) {
-      let items_per_page = ItemsPerPage::from_int(value);
-      assert_eq!(items_per_page.value(), value as u32);
+    fn pagination_should_set_default_to_size_when_size_is_greater_than_100(num in 1..i32::MAX, size in 101..=i32::MAX) {
+      let pagination = Pagination::from_int(num, size);
+      assert_eq!(pagination.num.value(), num as u32);
+      assert_eq!(pagination.size.value(), DEFAULT_ITEMS_PER_PAGE);
     }
 
     #[test]
-    fn items_per_page_should_be_default_when_value_is_negative(value in i32::MIN..=0i32) {
-      let items_per_page = ItemsPerPage::from_int(value);
-      assert_eq!(items_per_page.value(), DEFAULT_ITEMS_PER_PAGE);
+    fn pagination_should_calculate_offset(num in 1..i32::MAX, size in 1..=100) {
+      let pagination = Pagination::from_int(num, size);
+      assert_eq!(pagination.offset(), (size * (num - 1)) as u32);
     }
 
     #[test]
-    fn items_per_page_should_be_default_when_value_is_greater_than_100(value in 101i32..=i32::MAX) {
-      let items_per_page = ItemsPerPage::from_int(value);
-      assert_eq!(items_per_page.value(), DEFAULT_ITEMS_PER_PAGE);
+    fn pagination_should_calculate_limit(num in 1..i32::MAX, size in 1..=100) {
+      let pagination = Pagination::from_int(num, size);
+      assert_eq!(pagination.limit(), size as u32);
     }
   }
 }
