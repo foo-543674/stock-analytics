e3b11a37920208f726d7717d3337e5b55416e367
foo-543674 <foo.avoutar7654@gmail.com>
2025-01-29 03:53:15 +0000

    [refactor]fix router readable

--- api/src/routes/brand.rs
+++ api/src/routes/brand.rs
@@ -1,31 +1,28 @@
 use std::sync::Arc;
-use axum::{routing::{get, post}, Router};
-use crate::{https::brand::{get_brands::get_brands, post_brands::post_brands}, modules::brand::BrandModule};
+use axum::{extract::{Query, State}, response::IntoResponse, routing::{get, post}, Json, Router};
+use crate::{https::brand::{brand_json::BrandJson, get_brands::{get_brands, GetBrandListQueryParameter}, post_brands::{post_brands, PostBrandBody}}, modules::brand::BrandModule};
+
+async fn post_handler(
+  State(module): State<Arc<BrandModule>>,
+  body: Json<PostBrandBody>,
+) -> Result<Json<BrandJson>, impl IntoResponse> {
+  let usecase = module.resolve_register_brand_usecase();
+  return post_brands(body, &usecase).await;
+}
+
+async fn get_list_handler(
+  State(module): State<Arc<BrandModule>>,
+  param: Query<GetBrandListQueryParameter>,
+) -> Result<Json<Vec<BrandJson>>, impl IntoResponse> {
+  let query = module.resolve_brand_list_query();
+  return get_brands(param, &query).await;
+}
 
 pub fn brand_router(module: Arc<BrandModule>) -> Router {
   let routes = Router::new()
-    .route("/", post({
-      let module = Arc::clone(&module);
-      move |json| {
-        async move {
-          post_brands(json, module.resolve_register_brand_usecase()).await
-        }
-      }
-    }))
-    .route("/", get({
-      let module = Arc::clone(&module);
-      let query = module.resolve_brand_list_query().await;
-
-      move |param| {
-        let module = Arc::clone(&module);
-        async move {
-          match module.resolve_brand_list_query().await {
-            Ok(query) => get_brands(param, query).await,
-            Err(_) => Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR.into()),
-          }
-        }
-      }
-    }));
+    .route("/", post(post_handler))
+    .route("/", get(get_list_handler))
+    .with_state(module);
 
   return Router::new()
     .nest("/brands", routes);
