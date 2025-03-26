614bafda59502960d2cf7ad02322ccba2dd9e157
foo-543674 <foo.avoutar7654@gmail.com>
2025-03-26 17:17:40 +0000

    [feat]implement safety HttpError Handler

--- frontend/src/utils/Result.ts
+++ frontend/src/utils/Result.ts
@@ -1,6 +1,7 @@
 import * as NeverThrow from 'neverthrow';
 
 export type Result<T, E> = NeverThrow.Result<T, E>;
+export type ResultAsync<T, E> = NeverThrow.ResultAsync<T, E>;
 
 export const err = NeverThrow.err;
 export const errAsync = NeverThrow.errAsync;
@@ -9,3 +10,4 @@ export const ok = NeverThrow.ok;
 export const okAsync = NeverThrow.okAsync;
 
 export const fromPromise = NeverThrow.fromPromise;
+export const fromThrowable = NeverThrow.fromThrowable;
