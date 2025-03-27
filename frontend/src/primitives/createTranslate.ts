5db026cd0fc879f4730a60320660844cba262d57
foo-543674 <foo.avoutar7654@gmail.com>
2025-03-27 02:36:22 +0000

    [fix]use safety http request at createTranslate

--- frontend/src/primitives/createTranslate.ts
+++ frontend/src/primitives/createTranslate.ts
@@ -5,13 +5,20 @@ export type Replacement = Record<string, string>;
 export type Translate = (key: string, replacements?: Replacement) => string;
 
 export const createTranslate = (lang: Language): (() => Translate) => {
-  const [locales] = createResource(lang, fetchLocales);
+  const [locales] = createResource(
+    lang,
+    async lang => await fetchLocales(lang),
+  );
   const memoized = createMemo(() => {
-    if (locales.loading || locales.error) {
+    if (locales.loading) return () => '';
+
+    const fetched = locales();
+    if (!fetched || fetched.isErr()) {
+      //TODO: handle error
       return () => '';
     }
 
-    const dict = locales() as Record<string, string>;
+    const dict = fetched.value;
     return (key: string, replacements?: Replacement) => {
       const source = dict[key];
       if (!source) {
