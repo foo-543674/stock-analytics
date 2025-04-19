0b82ac8d66d27a960ed4d20601e3de362bc99335
foo-543674 <foo.avoutar7654@gmail.com>
2025-04-19 16:44:26 +0000

    [update]add error messages for validation

--- frontend/src/components/ValidationHintList.tsx
+++ frontend/src/components/ValidationHintList.tsx
@@ -9,13 +9,23 @@ export type ValidationHintListProps = Partial<{
   translate: Translate;
 }>;
 
+const translateKeys: Record<ValidationHintKey, string> = {
+  'validation.required': 'validationRequired',
+  'validation.duplicate': 'validationDuplicate',
+  'validation.max_length': 'validationMaxLength',
+  'validation.length_equals': 'validationLengthEquals',
+  'validation.numeric_only': 'validationNumericOnly',
+  'validation.resource_not_found': 'validationResourceNotFound',
+  'validation.ulid': 'validationUlid',
+};
+
 export const ValidationHintList = (props: ValidationHintListProps) => {
   return (
     <div>
       <For each={props.keys}>
         {key => (
           <div role="alert" class="alert alert-error alert-soft py-1 px-4 m-1">
-            <span class="text-sm">{props.translate?.(key)}</span>
+            <span class="text-sm">{props.translate?.(translateKeys[key])}</span>
           </div>
         )}
       </For>
